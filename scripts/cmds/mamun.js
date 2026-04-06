39696074eafa1dc6ee5b17dd0060c2c72f2b7899
sakib420bot <gathap417@gmail.com>
2026-04-06 14:44:09 +0600

    Update mamun.js

--- scripts/cmds/mamun.js
+++ scripts/cmds/mamun.js
@@ -4,14 +4,14 @@ const path = require("path");
 
 module.exports = {
   config: {
-    name: "mamun",
+    name: "『𝐒𝐀𝐊𝐈𝐁 𝐌𝐈𝐑𝐙𝐀』",
     version: "1.0.3",
-    author: "〲MAMUNツ࿐ T.T　o.O",
+    author: "sakib,
     role: 0,
     shortDescription: "Mamun Profile (No Prefix)",
     category: "Information",
     guide: {
-      en: "type mamun"
+      en: "type sakib"
     }
   },
 
@@ -20,23 +20,35 @@ module.exports = {
 
   onChat: async function ({ api, event }) {
     const msg = event.body?.toLowerCase();
-    if (!msg || msg !== "mamun") return;
+    if (!msg || msg !== "sakib") return;
 
     const profileText = 
 `╔════════════════════════════════════════╗
 ║ 🌌✨ 𝗣𝗥𝗢𝗙𝗜𝗟𝗘 𝗖𝗔𝗥𝗗 ✨🌌 ║
 ╠════════════════════════════════════════╣
-║ 🌟 Name       : 〲MAMUNツ࿐ T.T　o.O
-║ 🎂 Age        : 20
-║ 🆔 FF ID      : 3246615019
-║ 💌 Status     : Mingle
-║ 📍 Location   : Khulna, Bangladesh
-║ 🎮 Hobbies    : Gaming 🎮 | Coding 💻 | Music 🎧
-╠════════════════════════════════════════╣
-║ 🔗 Social Links:
-║ • Facebook  : https://www.facebook.com/profile.php?id=61587384024459
-║ • WhatsApp  : 01830981279
-║ • TikTok    : tm.mamun35
+║
+║[‎🤖] 𝐁𝐎𝐓 𝐀𝐃𝐌𝐈𝐍:-『𝐒𝐀𝐊𝐈𝐁 𝐌𝐈𝐑𝐙𝐀』
+║
+║[📝] 𝐁𝐈𝐎 𝐀𝐃𝐌𝐈𝐍 [👇]
+║
+║[মৃত্যুর আগে ও যদি তোমাকে পাওয়ার কোনো সুযোগ থাকে সেদিন ও আমি চিৎকার করে বলবো আজও আমি তোমাকে অসম্ভব ভালোবাসি।...!]
+║
+║[🏠] 𝐀𝐃𝐃𝐑𝐄𝐒𝐒 👉 :[𝙢𝙮𝙢𝙚𝙣𝙨𝙞𝙣𝙜𝙝]:[𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇] 
+║
+║[🕋] 𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍 👉 :[𝐈𝐒𝐋𝐀𝐌]
+║
+║[🚻] 𝐆𝐄𝐍𝐃𝐄𝐑 👉 :[𝐌𝐀𝐋𝐄]
+║
+║[💞] 𝐑𝐄𝐋𝐀𝐓𝐈𝐎𝐍𝐒𝐇𝐈𝐏 👉 :[𝐒𝐈𝐍𝐆𝐋𝐄]
+║
+║[🧑‍🔧] 𝐖𝐎𝐑𝐊 👉 :[𝐉𝐎𝐁]
+╚════════════════════════════════════════╝
+║_____________🅲🅾🅽🆃🅰🅲🆃_____________
+║[‎📞] 𝐖𝐇𝐀𝐓'𝐒 𝐀𝐏𝐏 👉:[https://wa.me/+8801790452366]
+║
+║
+║[‎🌍] 𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐈𝐃 (❶)💥 : https://m.me/Mir za
+║
 ╠════════════════════════════════════════╣
 ║ 🎯 Game      : Free Fire 🔫
 ╚════════════════════════════════════════╝`;
